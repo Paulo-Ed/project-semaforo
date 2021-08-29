ebcbfc394d09dc764f84b52fc5ce2416a8a2a308
Paulo Santos <pauloed_@outlook.com>
2021-08-29 16:49:42 -0300

    Criando funções para acender e apagar o semaforo

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+
+function acendeVermelho(){
+    document.getElementById('vermelho').style.backgroundColor = '#ff0000'
+    document.getElementById('amarelo').style.backgroundColor = '#828200'
+    document.getElementById('verde').style.backgroundColor = '#002a00'
+    
+}
+
+function acendeAmarelo(){
+    document.getElementById('vermelho').style.backgroundColor = '#530000'
+    document.getElementById('amarelo').style.backgroundColor = '#ffd700'
+    document.getElementById('verde').style.backgroundColor = '#002a00'
+    
+}
+
+function acendeVerde(){
+    document.getElementById('vermelho').style.backgroundColor = '#530000'
+    document.getElementById('amarelo').style.backgroundColor = '#828200'
+    document.getElementById('verde').style.backgroundColor = '#00ff00'
+    
+}
